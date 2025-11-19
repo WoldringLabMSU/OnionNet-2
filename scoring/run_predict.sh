9fccc0b12a8ca318275e2f73cc12b07e7126f8d0
Joelle Eaves <jeaves@msu.edu>
2025-11-19 15:23:10 -0500

    Modified to run through directory recursively

--- scoring/run_predict.sh
+++ scoring/run_predict.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-rec_fpath=../samples/1bcu/1bcu_protein.pdb
-lig_fpath=../samples/1bcu/1bcu_ligand.pdb
-out_fpath=1bcu_pred-pKd.csv
+
 
 scaler_fpath=../models/train_scaler.scaler
 model_fpath=../models/62shell_saved-model.h5
+dir_list=$(ls /mnt/scratch/jeaves/CASF-2016/coreset)
+# dir_list=$(ls /mnt/scratch/jeaves/OnionNet-2/samples)
 
-python predict.py \
-    -rec_fpath $rec_fpath \
-    -lig_fpath $lig_fpath \
-    -shape 84,124,1 \
-    -scaler $scaler_fpath \
-    -model $model_fpath \
-    -shells 62 \
-    -out_fpath $out_fpath
+for item in $dir_list; do
+    rec_fpath=/mnt/scratch/jeaves/CASF-2016/coreset/${item}/${item}_protein.pdb
+    lig_pdb_fpath=/mnt/scratch/jeaves/CASF-2016/coreset/${item}/${item}_ligand.pdb
+    lig_sdf_fpath=/mnt/scratch/jeaves/CASF-2016/coreset/${item}/${item}_ligand.sdf
+    out_fpath=/mnt/scratch/jeaves/CASF-2016/coreset/${item}/pred.csv
+
+    if [ ! -f "$ligand_pdb_fpath" ]; then
+        obabel "$lig_sdf_fpath" -O "$lig_pdb_fpath"
+    fi
+
+    python predict.py \
+        -rec_fpath $rec_fpath \
+        -lig_fpath $lig_pdb_fpath \
+        -shape 84,124,1 \
+        -scaler $scaler_fpath \
+        -model $model_fpath \
+        -shells 62 \
+        -out_fpath $out_fpath
+done
+
